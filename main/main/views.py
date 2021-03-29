2d1ffe6827be4b079e76c5fcf2dd63f20fedd387
Sokolov Aleksandr <a.sokolov@RTK-IT.ru>
2021-03-29 18:52:15 +0300

    pep8 fixes

--- main/main/views.py
+++ main/main/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView, UpdateView
-from django.contrib.auth.models import User
+
 from .forms import ProfileForm
-from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Good
 
 
