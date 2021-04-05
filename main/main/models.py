45a6398c856ecd75a12c884bbb16c7a76885deed
Sokolov Aleksandr <a.sokolov@RTK-IT.ru>
2021-04-05 18:46:46 +0300

    fixes

--- main/main/models.py
+++ main/main/models.py
@@ -135,3 +135,4 @@ class Profile(models.Model):
     def create_user_profile(sender, instance, created, **kwargs):
         if created:
             instance.groups.add(Group.objects.get(name='common users'))
+            Profile.objects.create(user_id=instance.id)
