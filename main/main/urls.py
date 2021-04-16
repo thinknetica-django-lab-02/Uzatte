0b5cca983c78edf01ef1c758d251aacce964ccdb
Sokolov Aleksandr <a.sokolov@RTK-IT.ru>
2021-04-16 19:01:06 +0300

    CACHE_TTL set in settings

--- main/main/urls.py
+++ main/main/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -20,13 +21,15 @@ from django.urls import include, path
 from django.views.decorators.cache import cache_page
 from . import views
 
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', views.index, name='index'),
-    path('goods/', cache_page(60 * 15)(views.GoodList.as_view()), name='goods'),
+    path('goods/', cache_page(CACHE_TTL)(views.GoodList.as_view()), name='goods'),
     path('goods/add/', views.GoodCreate.as_view(), name='good-add'),
-    path('goods/<pk>/', cache_page(60 * 15)(views.GoodDetail.as_view()), name='good-detail'),
+    path('goods/<pk>/', cache_page(CACHE_TTL)(views.GoodDetail.as_view()), name='good-detail'),
     path('goods/<pk>/edit', views.GoodEdit.as_view(), name='good-edit'),
     path('accounts/profile/', views.ProfileUpdate.as_view(), name='profile'),
     path('accounts/profile/phone_confirm', views.phone_number_confirmation,
