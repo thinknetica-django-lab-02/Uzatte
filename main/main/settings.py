9ca712f98fe55b053c7cc1caf61148acf63d67f0
Sokolov Aleksandr <a.sokolov@RTK-IT.ru>
2021-04-26 13:47:52 +0300

    added static_root, made collectstatic command, created another superuser

--- main/main/settings.py
+++ main/main/settings.py
@@ -183,6 +183,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Celery Configuration Options
 CELERY_TIMEZONE = "Europe/Moscow"
