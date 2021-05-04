060bd6e4adb3cd23f8dc1ea995e43c8400dc2629
Sokolov Aleksandr <a.sokolov@RTK-IT.ru>
2021-05-04 12:18:42 +0300

    fixes

--- main/main/urls.py
+++ main/main/urls.py
@@ -18,7 +18,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
 from django.urls import include, path
-# from django.views.decorators.cache import cache_page
 
 from . import views
 
